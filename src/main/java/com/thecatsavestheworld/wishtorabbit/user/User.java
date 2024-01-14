49bd937b4819f9cee0b723b6007bfb1eb30d3834
zpqmdh <ysys1130@gmail.com>
2024-01-14 18:23:19 +0900

    feat: Add user class

--- src/main/java/com/thecatsavestheworld/wishtorabbit/user/User.java
+++ src/main/java/com/thecatsavestheworld/wishtorabbit/user/User.java
@@ -1,2 +1,11 @@
-package com.thecatsavestheworld.wishtorabbit.user;public class User {
+package com.thecatsavestheworld.wishtorabbit.user;
+
+public class User {
+    private int uid;
+    private String nickname;
+    private String password;
+    public User(String nickname, String password) {
+        this.nickname = nickname;
+        this.password = password;
+    }
 }
