7f6e41b790ac3a772b6c4d63767854ddaeb5ddb8
zpqmdh <ysys1130@gmail.com>
2024-01-14 18:04:27 +0900

    feat: Set spring boot

---
+++ src/main/java/com/thecatsavestheworld/wishtorabbit/WishtorabbitApplication.java
@@ -0,0 +1,13 @@
+package com.thecatsavestheworld.wishtorabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishtorabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishtorabbitApplication.class, args);
+	}
+
+}
